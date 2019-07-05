64dc4a95f5ad83a644e799bc8efd329708ac0d45
Javier Quiroz <javierqq@gmail.com>
2019-07-05 15:09:59 -0400

    test get_free_page output

--- exercises/lesson04/2/rs/src/test.c
+++ exercises/lesson04/2/rs/src/test.c
@@ -2,7 +2,7 @@
 #include "entry.h"
 #include "sched.h"
 
-int int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) {
 
   struct task_struct *p;
   p = (struct task_struct *)get_free_page();
